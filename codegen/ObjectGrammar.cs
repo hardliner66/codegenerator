33213af15ab3b914ee33b383ca37282c471832c9
Steve Biedermann <steve.biedermann@mquadr.at>
2017-11-21 16:29:44 +0100

    (+) added new syntax

--- codegen/ObjectGrammar.cs
+++ codegen/ObjectGrammar.cs
@@ -16,6 +16,7 @@ namespace Codegen
             //var identifier = new RegexBasedTerminal("identifier", @"\b((?!list)[a-zA-Z0-9_])+\b");
             var value = new RegexBasedTerminal("name", @"\b[a-zA-Z0-9_\.]+\b");
             var str = new QuotedValueLiteral("value", "\"", TypeCode.String);
+            var num = new NumberLiteral("num", NumberOptions.IntOnly);
 
             var external = new NonTerminal("external");
             var line = new NonTerminal("line");
@@ -31,22 +32,39 @@ namespace Codegen
             var attribute_kv = new NonTerminal("attribute_kv");
             var attribute_value = new NonTerminal("attribute_value");
             var attribute_flag = new NonTerminal("attribute_flag");
+            var intAttributeList = new NonTerminal("intAttributeList");
+            var intAttributes = new NonTerminal("intAttributes");
+            var intAttribute = new NonTerminal("intAttribute");
+            var intAttribute_kv = new NonTerminal("intAttribute_kv");
             var optional_flag = new NonTerminal("optional_flag");
             var optional_flag_opt = new NonTerminal("optional_flag_opt");
             var default_value = new NonTerminal("default_value");
             var default_value_opt = new NonTerminal("default_value_opt");
             var type = new NonTerminal("type");
-            var list = new NonTerminal("list");
             var comma = ToTerm(",", "comma");
+            var comment = new RegexBasedTerminal("comment", @"\/\/.*");
+            var comment_opt = new NonTerminal("comment")
+            {
+                Rule = Empty | comment
+            };
 
-            CommentTerminal SingleLineComment = new CommentTerminal("SingleLineComment", "//", "\r", "\n", "\u2085", "\u2028", "\u2029");
-            //CommentTerminal DelimitedComment = new CommentTerminal("DelimitedComment", "/*", "*/");
-            NonGrammarTerminals.Add(SingleLineComment);
-            //NonGrammarTerminals.Add(DelimitedComment);
+
+            var list = new NonTerminal("list");
+            var @enum = new NonTerminal("enum");
+            var dict = new NonTerminal("dict");
+
+            @enum.Rule = ToTerm("enum") + identifier + intAttributeList;
 
             external.Rule = ToTerm("external") + identifier + attributeList_opt;
 
-            line.Rule = external | @object;
+            line.Rule = comment | @enum | external | @object;
+          
+            intAttribute_kv.Rule = identifier + "=" + num;
+            intAttribute.Rule = intAttribute_kv | attribute_flag;
+            intAttributes.Rule = MakePlusRule(intAttributes, comma, intAttribute);
+
+            intAttributeList.Rule = "[" + intAttributes + "]";
+            
 
             attribute_value.Rule = value | str;
 
@@ -59,7 +77,8 @@ namespace Codegen
             attributeList_opt.Rule = Empty | attributeList;
 
             list.Rule = "List" + identifier;
-            type.Rule = identifier | list;
+            dict.Rule = "Map" + identifier + identifier;
+            type.Rule = identifier | list | dict;
 
             default_value.Rule = ToTerm("=") + attribute_value;
             default_value_opt.Rule = Empty | default_value;
@@ -67,20 +86,20 @@ namespace Codegen
             optional_flag.Rule = "?";
             optional_flag_opt.Rule = Empty | optional_flag;
 
-            property.Rule = optional_flag_opt + identifier + ":" + type + default_value_opt + attributeList_opt;
+            property.Rule = optional_flag_opt + identifier + ":" + type + default_value_opt + attributeList_opt + comment_opt;
 
-            properties.Rule = MakePlusRule(properties, property);
+            properties.Rule = MakeStarRule(properties, property);
 
-            @object.Rule = ToTerm("object") + identifier + attributeList_opt + "{" + properties + "}";
+            @object.Rule = ToTerm("object") + identifier + attributeList_opt + "{" + comment_opt + properties + "}" + comment_opt;
 
             objectList.Rule = MakePlusRule(objectList, line);
 
             Root = objectList;
 
-            MarkPunctuation("=", "[", "]", ":", "{", "}", ";", "list", "object", "external");
+            MarkPunctuation("=", "[", "]", ":", "{", "}", "//", "?", ";", "list", "map", "object", "external");
         }
 
-        public bool isValid(string sourceCode, Grammar grammar)
+        public bool IsValid(string sourceCode, Grammar grammar)
         {
             LanguageData language = new LanguageData(grammar);
             Parser parser = new Parser(language);
@@ -89,7 +108,7 @@ namespace Codegen
             return root != null;
         }
 
-        public ParseTreeNode getRoot(string sourceCode, Grammar grammar)
+        public ParseTreeNode GetRoot(string sourceCode, Grammar grammar)
         {
             LanguageData language = new LanguageData(grammar);
             Parser parser = new Parser(language);
@@ -98,14 +117,14 @@ namespace Codegen
             return root;
         }
 
-        public void dispTree(ParseTreeNode node, int level)
+        public void DispTree(ParseTreeNode node, int level)
         {
             for (int i = 0; i < level; i++)
                 Console.Write("  ");
             Console.WriteLine(node);
 
             foreach (ParseTreeNode child in node.ChildNodes)
-                dispTree(child, level + 1);
+                DispTree(child, level + 1);
         }
     }
 }
