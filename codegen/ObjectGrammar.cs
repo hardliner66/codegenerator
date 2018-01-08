4e5743368ab90d1adbc8dcdc97e7b8af36ae7dc4
Steve Biedermann <steve.biedermann@mquadr.at>
2018-01-08 09:43:00 +0100

    (c) allow empty objects

--- codegen/ObjectGrammar.cs
+++ codegen/ObjectGrammar.cs
@@ -25,6 +25,7 @@ namespace Codegen
             var objectList = new NonTerminal("objectList");
             var properties = new NonTerminal("properties");
             var property = new NonTerminal("property");
+            var optional_properties = new NonTerminal("optional_properties");
             var attributeList_opt = new NonTerminal("attributeList_opt");
             var attributeList = new NonTerminal("attributeList");
             var attributes = new NonTerminal("attributes");
@@ -90,7 +91,9 @@ namespace Codegen
 
             properties.Rule = MakeStarRule(properties, property);
 
-            @object.Rule = ToTerm("object") + identifier + attributeList_opt + "{" + comment_opt + properties + "}" + comment_opt;
+            optional_properties.Rule = Empty | properties;
+
+            @object.Rule = ToTerm("object") + identifier + attributeList_opt + "{" + comment_opt + optional_properties + "}" + comment_opt;
 
             objectList.Rule = MakePlusRule(objectList, line);
 
